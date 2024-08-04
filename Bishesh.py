973c6bebdc7c4ec205683397919b61e61bcca91f
Bishesh <155248290+0x4ef8@users.noreply.github.com>
2024-08-04 21:03:24 +0545

    Update Bishesh.py

--- Bishesh.py
+++ Bishesh.py
@@ -1,10 +1,1 @@
-import sys
-import os
-
-so_file_directory = "sc"
-sys.path.append(so_file_directory)
-
-import ula
-
-result = ula.example_function()
-print(result)
+import sc.ula.cpython-311 as ula
